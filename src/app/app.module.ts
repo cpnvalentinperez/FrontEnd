5f2418429f64a24b2057d6eb0325dc4eeebcfe7f
vperezarrascaeta <vperezarrascaeta@epe.santafe.gov.ar>
2023-06-28 10:41:18 -0300

    Front y environment andando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,9 @@ import { EditeducacionComponent } from './components/educacion/editeducacion.com
 import { EditSkillComponent } from './components/habilidades/edit-skill.component';
 import { NewSkillComponent } from './components/habilidades/new-skill.component';
 import { EditAcercaDeComponent } from './components/acercade/edit-acerca-de.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -54,7 +57,9 @@ import { EditAcercaDeComponent } from './components/acercade/edit-acerca-de.comp
     BrowserAnimationsModule,
     FormsModule,
     NgCircleProgressModule.forRoot({}),
-    HttpClientModule
+    HttpClientModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideStorage(() => getStorage())
   ],
   providers: [interceptorService],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'frontendvp-b302b',
+    appId: '1:969077010632:web:eddaeaf368d8347c1775fd',
+    storageBucket: 'frontendvp-b302b.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyDPnw5kySceEN1Jz6c0eXRT6dMVzVDwuOQ',
+    authDomain: 'frontendvp-b302b.firebaseapp.com',
+    messagingSenderId: '969077010632',
+  },
 
     production: false,
     URL: 'http://localhost:8080/'
